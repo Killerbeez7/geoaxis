2c8ce036bd232a15671b814f5d5f0b55e3c76b31
Plamen Tsvetkov <62841682+Killerbeez7@users.noreply.github.com>
2026-05-05 20:24:43 +0300

    Add helpers for "Polezno/Articles"

---
+++ geoaxis-web/src/utils/polezno/helpers.ts
@@ -0,0 +1,16 @@
+import { HELPFUL_ARTICLES, type HelpfulArticleSection } from "@/config/polezno/articles";
+
+export function getArticleBySlug(slug: string) {
+  return HELPFUL_ARTICLES.find((article) => article.slug === slug);
+}
+
+export function getArticlesBySection(section: HelpfulArticleSection) {
+  return HELPFUL_ARTICLES.filter((article) => article.section === section);
+}
+
+export function getRelatedArticles(currentSlug: string, limit = 2) {
+  return HELPFUL_ARTICLES.filter((article) => article.slug !== currentSlug).slice(
+    0,
+    limit
+  );
+}
